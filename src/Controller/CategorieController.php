14f5053c9a510826869da650341b722eb50a2df7
Jean-Paul PICHON <jpaul_pichon@hotmail.com>
2023-03-05 14:45:57 +0100

    AJOUT action ficheCategorie() + vue pour afficher detail d'une categorie de rencontre

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,5 +18,15 @@ class CategorieController extends AbstractController
             'categories' => $categories,
         ]);
     }
+
+    #[Route('/categorie/{id}', name: 'app_categorie', methods: ["GET"])]
+    public function ficheCategorie(Categorie $categorie): Response
+    {
+        // mise en oeuvre du ParamConverter
+        return $this->render('categorie/ficheCategorie.html.twig', [
+            'categorie' => $categorie,
+        ]);
+    }
+
 
 }
