862c423489aa1efb90cc18b89d271ff97e3029df
Jean-Paul PICHON <jpaul_pichon@hotmail.com>
2023-03-05 14:27:52 +0100

    AJOUT CategorieController.php + action pour lister toutes les categories + vue

---
+++ src/Controller/CategorieController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategorieRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CategorieController extends AbstractController
+{
+    #[Route('/categories', name: 'app_categories', methods: ["GET"])]
+    public function listeCategories(CategorieRepository $repo): Response
+    {
+        $categories = $repo->findAll();
+        return $this->render('categorie/listeCategories.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+}
